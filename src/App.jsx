a61b854faccca7dd5eca51f5b123c2da1afff111
Chandrasura25 <abdulganiy1624demilade@gmail.com>
2023-08-09 16:54:23 +0100

    progress bar

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
 import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import Home from "./pages/Home";
-
+import ProgressBar from "./components/ProgressBar";
 function App() {
   return (
     <>
+      <ProgressBar />
       <Routes>
-        <Route path="/" element={<Home/>} />
+        <Route path="/" element={<Home />} />
         <Route path="/about" element={<h1>About</h1>} />
-        
       </Routes>
     </>
   );
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,6 +4,7 @@ const Home = () => {
   return (
     <div className="container-fluid p-0 m-0">
         <Navbar />
+        <a href="/about">About</a>
     </div>
   )
 }
