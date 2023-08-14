919a9c99d985dfae775b1d883eae926925cccb94
Chandrasura25 <abdulganiy1624demilade@gmail.com>
2023-08-14 18:54:19 +0100

    slider

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,30 +1,37 @@
-import aboutStyle from "../styles/About.module.css";
-import { motion } from "framer-motion";
-import { AboutText } from "../utils/constant";
-import { Variant, textVariants, transition,slideContainerVariants } from '../utils/animationVariants'; 
+import { motion, AnimatePresence } from 'framer-motion';
+import aboutStyle from '../styles/About.module.css';
+import { AboutText } from '../utils/constant';
+import { Variant, textVariants, transition, slideContainerVariants } from '../utils/animationVariants';
+
 const About = () => {
+
   return (
-    <div className={aboutStyle.container}> 
-      <motion.div className={aboutStyle.slider} variants={slideContainerVariants}>
-        {AboutText.map((text, index) => (
-          <motion.div
-            className={`${aboutStyle.slide} ${aboutStyle["slide" + text.id]}`} 
-            variants={Variant}
-            initial="hidden"
-            animate="visible"
-            exit="hidden"
-            key={index}
-          >
-            <div className={aboutStyle.caption}>
-              <motion.h2 variants={textVariants} transition={transition}>
-                {text.title}
-              </motion.h2>
-              <p>{text.description}</p>
-            </div>
-          </motion.div>
-        ))}
+    <div className={aboutStyle.container}>
+      <motion.div
+        className={aboutStyle.slider}
+        variants={slideContainerVariants}
+      >
+        <AnimatePresence >
+          {AboutText.map((text, index) => (
+            <motion.div
+              className={`${aboutStyle.slide} ${aboutStyle["slide" + text.id]}`}
+              variants={Variant}
+              initial="hidden"
+              animate="visible"
+              exit="hidden"
+              key={index}
+            >
+              <div className={aboutStyle.caption}>
+                <motion.h2 variants={textVariants} transition={transition}>
+                  {text.title}
+                </motion.h2>
+                <p>{text.description}</p>
+              </div>
+            </motion.div>
+          ))}
+        </AnimatePresence>
       </motion.div>
-    </div>  
+    </div>
   );
 };
 
--- src/utils/animationVariants.js
+++ src/utils/animationVariants.js
@@ -11,9 +11,75 @@ export const textVariants = {
 
 export const transition = {
     duration: 0.5, // Transition duration in seconds
+    ease: "easeInOut", // Transition easing
+    delay: 0.2, // Delay before transition
 };
 
 export const slideContainerVariants = {
     hidden: { opacity: 0 },
     visible: { opacity: 1, transition: { staggerChildren: 0.1 } },
 };
+export const slideVariants = {
+    hiddenRight: {
+        x: "100%",
+        opacity: 0,
+    },
+    hiddenLeft: {
+        x: "-100%",
+        opacity: 0,
+    },
+    visible: {
+        x: "0",
+        opacity: 1,
+        transition: {
+            duration: 1,
+        },
+    },
+    exit: {
+        opacity: 0,
+        scale: 0.8,
+        transition: {
+            duration: 0.5,
+        },
+    },
+};
+export const slidersVariants = {
+    hover: {
+        scale: 1.2,
+        backgroundColor: "#ff00008e",
+    },
+};
+export const dotsVariants = {
+    initial: {
+        y: 0,
+    },
+    animate: {
+        y: -10,
+        scale: 1.2,
+        transition: { type: "spring", stiffness: 1000, damping: "10" },
+    },
+    hover: {
+        scale: 1.1,
+        transition: { duration: 0.2 },
+    },
+};
+
+//   const handleNext = () => {
+//     setDirection("right");
+//     setCurrentIndex((prevIndex) =>
+//       prevIndex + 1 === images.length ? 0 : prevIndex + 1
+//     );
+//   };
+
+//   const handlePrevious = () => {
+//     setDirection("left");
+
+//     setCurrentIndex((prevIndex) =>
+//       prevIndex - 1 < 0 ? images.length - 1 : prevIndex - 1
+//     );
+//   };
+
+//   const handleDotClick = (index) => {
+//     setDirection(index > currentIndex ? "right" : "left");
+//     setCurrentIndex(index);
+//   };
