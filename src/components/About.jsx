444ddf48a22654c2c466679192ee71126ded4e9d
Chandrasura25 <abdulganiy1624demilade@gmail.com>
2023-08-15 18:45:04 +0100

    about done

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,36 +1,36 @@
 import { motion, AnimatePresence } from 'framer-motion';
-// import aboutStyle from '../styles/About.module.css';
-import { Variant, textVariants, transition } from '../utils/animationVariants';
-import { useState, useEffect } from 'react';  
+import { useEffect, useState } from 'react';
+import aboutStyle from '../styles/About.module.css';
 import { AboutText } from '../utils/constant';
-import "../styles/about.css"
+import { Variant, textVariants, transition} from '../utils/animationVariants';
+
 const About = () => {
   const [activeSlide, setActiveSlide] = useState(0);
 
   useEffect(() => {
     const timer = setInterval(() => {
       setActiveSlide(prevSlide => (prevSlide + 1) % AboutText.length);
-    }, 5000);
+    }, 10000);
 
     return () => {
       clearInterval(timer);
     };
   }, []);
   return (
-    <div className='container'>
+    <div className={aboutStyle.container}>
       <motion.div
-        className='slider'
+        className={aboutStyle.slider}
       >
         <AnimatePresence>
-          {AboutText.map((text, index) => (
+        {AboutText.map((text, index) => (
             <motion.div
-              className={`slide ${
-                index === activeSlide ? 'active' : ''
-              }`}
+            className={`${aboutStyle.slide} ${
+              index === activeSlide ? aboutStyle.active : ''
+            } ${aboutStyle["slide" + text.id]} `}
               variants={Variant}
               key={index}
             >
-              <div className='caption'>
+              <div className={aboutStyle.caption}>
                 <motion.h2 variants={textVariants} transition={transition}>
                   {text.title}
                 </motion.h2>
