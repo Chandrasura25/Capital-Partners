9370fbb4209456dc9ceb62e332c3623047445fd6
Chandrasura25 <abdulganiy1624demilade@gmail.com>
2023-08-15 20:06:26 +0100

    vision

--- src/components/Vision.jsx
+++ src/components/Vision.jsx
@@ -1,14 +1,30 @@
-import { motion } from "framer-motion"
+import { motion } from "framer-motion";
 import visionStyle from "../styles/Vision.module.css";
-import { textVariant } from "../utils/animationVariants";
+import { textVariant, textVariants } from "../utils/animationVariants";
 
 const Vision = () => {
   return (
     <motion.section className={visionStyle.vision}>
-      <motion.h2 variants={textVariant()} className={visionStyle.titleText}>Our <span>Vision</span></motion.h2>
-      Our vision at Capital Partners Investment PLC is to be the leading investment company, recognized for our innovative strategies, exceptional performance, and unwavering commitment to our clients. We strive to be the investment firm of choice, consistently exceeding expectations and setting new industry standards. By leveraging our expertise, fostering long-term relationships, and embracing cutting-edge technology, we aim to empower individuals, institutions, and communities to thrive financially and make a positive impact on the world.
+      <motion.h2 variants={textVariant(0.3)} className={visionStyle.titleText}>
+        Our <span>Vision</span>
+      </motion.h2>
+      <div className={visionStyle.content}>
+        <motion.p variants={textVariants} className={visionStyle.visionText}>
+          Our vision at Capital Partners Investment PLC is to be the leading
+          investment company, recognized for our innovative strategies,
+          exceptional performance, and unwavering commitment to our clients.{" "}
+          <br />
+          <br /> We strive to be the investment firm of choice, consistently
+          exceeding expectations and setting new industry standards. <br />
+          <br /> By
+          leveraging our expertise, fostering long-term relationships, and
+          embracing cutting-edge technology, we aim to empower individuals,
+          institutions, and communities to thrive financially and make a
+          positive impact on the world.
+        </motion.p>
+      </div>
     </motion.section>
-  )
-}
+  );
+};
 
-export default Vision
+export default Vision;
