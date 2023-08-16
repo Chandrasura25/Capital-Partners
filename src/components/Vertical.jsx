ac5968cc308231476f4bd3416eee0266793a6361
Chandrasura25 <abdulganiy1624demilade@gmail.com>
2023-08-16 16:53:54 +0100

    vertical timeline

--- src/components/Vertical.jsx
+++ src/components/Vertical.jsx
@@ -1,16 +1,18 @@
 import {  VerticalTimelineElement }  from 'react-vertical-timeline-component';
 import "react-vertical-timeline-component/style.min.css";
 import PropTypes from "prop-types";
+import investStyles from "../styles/Invest.module.css";
 const Vertical = ({id,title,content, icon}) => {
   return (
     <VerticalTimelineElement className="vertical-timeline-element--work"
     contentStyle={{ background: '#43a5f6', color: '#fff' }}
     contentArrowStyle={{ borderRight: '7px solid  #43a5f6' }} 
     iconStyle={{ background: '#43a5f6', color: '#fff' }}
-    icon={icon}>
-      <h3 className="vertical-timeline-element-title">0{id}</h3>
-      <h4 className="vertical-timeline-element-subtitle">{title}</h4>
-      <p>{content}</p>
+    icon={<i className={icon}></i>}>
+      <h3 className={investStyles.title}>0{id}</h3>
+      <h4 className={investStyles.subtitle}>{title}</h4>
+      <p className={investStyles.content}>{content}</p>
+      <button className={investStyles.more}>Read More</button>
     </VerticalTimelineElement>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,8 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
+// fontawesome
+import "font-awesome/css/font-awesome.min.css";
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
