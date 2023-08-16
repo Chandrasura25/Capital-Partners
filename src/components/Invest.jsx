a936f5eb630959e50f6c225b4e7cb31b6b304c06
Chandrasura25 <abdulganiy1624demilade@gmail.com>
2023-08-16 17:19:31 +0100

    invest done

--- src/components/Invest.jsx
+++ src/components/Invest.jsx
@@ -1,22 +1,27 @@
-import {motion} from "framer-motion";
+import { motion } from "framer-motion";
 import investStyles from "../styles/Invest.module.css";
 import { textVariant, fadeIn } from "../utils/animationVariants";
-import { VerticalTimeline }  from 'react-vertical-timeline-component';
-import 'react-vertical-timeline-component/style.min.css';
+import { VerticalTimeline } from "react-vertical-timeline-component";
+import "react-vertical-timeline-component/style.min.css";
 import Vertical from "./Vertical";
 import { investText } from "../utils/constant";
 const Invest = () => {
   return (
     <motion.section id="invest" className={investStyles.container}>
       <motion.div>
-        <motion.h2 variants={textVariant()} className={investStyles.titleText}>WHY YOU SHOULD <span>INVEST WITH US</span></motion.h2>
+        <motion.h2 variants={textVariant()} className={investStyles.titleText}>
+          WHY YOU SHOULD <span>INVEST WITH US</span>
+        </motion.h2>
         <motion.h4 variants={textVariant()} className={investStyles.subText}>
           Capital Partners Investment Plc:{" "}
           <span>
             Unlocking Diverse Investment Opportunities for a Thriving Future
           </span>
         </motion.h4>
-        <motion.p className={investStyles.top} variants={fadeIn("up", "spring", 0.5, 0.75)}>
+        <motion.p
+          className={investStyles.top}
+          variants={fadeIn("up", "spring", 0.5, 0.75)}
+        >
           In a rapidly evolving global economy, investors seek opportunities
           that combine stability, growth potential, and diversification. Enter
           Capital Partners Investment Plc, an innovative and forward-thinking
@@ -25,23 +30,21 @@ const Invest = () => {
           transportation, real estate, health, oil and gas, education,
           technology, and food, Capital Partners Investment Plc presents an
           enticing proposition for individuals and businesses seeking to
-          maximize their investment potential. 
+          maximize their investment potential.
           <i>
             This is why Capital Partners Investment Plc should be at the top of
             every investor`s list.
           </i>
         </motion.p>
       </motion.div>
-     <motion.div>
-        <VerticalTimeline>
-         {
-            investText.map((invest, index) => (
-              <Vertical key={index} {...invest} index={index}/>
-            ))
-         }
-        </VerticalTimeline>
-     </motion.div>
       <motion.div>
+        <VerticalTimeline>
+          {investText.map((invest, index) => (
+            <Vertical key={index} {...invest} index={index} />
+          ))}
+        </VerticalTimeline>
+      </motion.div>
+      <motion.div className={investStyles.bottom}>
         Capital Partners Investment stands as a beacon of opportunity for
         investors seeking a diversified and forward-thinking investment company.
         We position ourself as a catalyst for success. Whether you are an
