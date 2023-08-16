f2c80eee1717cb2a41b8e2a3b570034c7bd8a0c6
Chandrasura25 <abdulganiy1624demilade@gmail.com>
2023-08-16 09:19:09 +0100

    pillars and core sector

--- src/components/Pillar.jsx
+++ src/components/Pillar.jsx
@@ -1,27 +1,35 @@
+import { motion } from "framer-motion";
+import { textVariant, fadeIn } from "../utils/animationVariants";
+import pillarStyles from "../styles/Pillar.module.css";
 const Pillar = () => {
   return (
     <>
-      <div className="titleText">
-        OUR CORE INVESTMENT SECTORS; THE 7 PILLARS OF OUR INVESTMENT
-      </div>
-      <div className="top">
-        Our investment portfolio encompasses a wide range of sectors, providing
-        opportunities for both domestic and international investors. We believe
-        in the power of strategic investments to drive economic development and
-        improve the lives of individuals. As such, we have carefully selected
-        sectors that are essential to Nigeria`s growth and have significant
-        potential for long-term profitability.
-      </div>
-      <div className="bottom">
-        At Capital Partners Investment Plc, we combine our extensive industry
-        knowledge, market insights, and investment expertise to identify and
-        execute value-driven opportunities. Our team of seasoned professionals
-        is committed to delivering superior returns while upholding the highest
-        standards of corporate governance and social responsibility. We invite
-        you to explore the possibilities of partnering with Capital Partners
-        Investment Plc, where your financial goals meet our strategic vision,
-        creating a path to mutual success.
-      </div>
+      <motion.section className={pillarStyles.pillar}>
+        <div className={pillarStyles.titleText}>
+          <motion.p variants={textVariant()}>
+            OUR CORE <span>INVESTMENT SECTORS</span>
+          </motion.p>
+        </div>
+        <motion.div variants={fadeIn("", "", 0.1, 1)} className={pillarStyles.top}>
+          <motion.p variants={textVariant()}>Our investment portfolio encompasses a wide range of sectors,
+          providing opportunities for both domestic and international investors.
+          We believe in the power of strategic investments to drive economic
+          development and improve the lives of individuals. As such, we have
+          carefully selected sectors that are essential to Nigeria`s growth and
+          have significant potential for long-term profitability.</motion.p>
+        </motion.div>
+        <br /> <span>THE 7 PILLARS OF OUR INVESTMENT</span>
+        <div className="bottom">
+          At Capital Partners Investment Plc, we combine our extensive industry
+          knowledge, market insights, and investment expertise to identify and
+          execute value-driven opportunities. Our team of seasoned professionals
+          is committed to delivering superior returns while upholding the
+          highest standards of corporate governance and social responsibility.
+          We invite you to explore the possibilities of partnering with Capital
+          Partners Investment Plc, where your financial goals meet our strategic
+          vision, creating a path to mutual success.
+        </div>
+      </motion.section>
     </>
   );
 };
