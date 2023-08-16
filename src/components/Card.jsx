05cdce17a3b8a349d9748172cbf7c3e1d6345bed
Chandrasura25 <abdulganiy1624demilade@gmail.com>
2023-08-16 09:44:32 +0100

    cards

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,10 +1,31 @@
-import motion from 'framer-motion';
+import { motion } from 'framer-motion'; 
 import { fadeIn } from '../utils/animationVariants';
-const Card = ({index}) => {
+import PropTypes from 'prop-types'; 
+import cardStyles from '../styles/Card.module.css';
+const Card = ({ id, name }) => {
   return (
-    <motion.div variants={fadeIn("up", "spring", index * 0.5, 0.75)}>
-        </motion.div>
-  )
-}
-
-export default Card
+    <motion.div
+      initial="hidden"
+      animate="visible"
+      variants={fadeIn("up", "spring", id * 0.5, 0.75)}
+      className={cardStyles.drop}
+    >
+      <div class="drop" style="--clr: #">
+        <div class="content">
+          <h2>01</h2>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt,
+            saepe?
+          </p>
+          <a href="#">Read More</a>
+        </div>
+      </div>
+    </motion.div>
+  );
+};
+// Add prop validation
+Card.propTypes = {
+    index: PropTypes.number.isRequired,
+    name: PropTypes.string.isRequired, 
+  };
+export default Card;
