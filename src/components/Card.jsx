70b9bce7165a07337cd869269b4368e9ce6e6772
Chandrasura25 <abdulganiy1624demilade@gmail.com>
2023-08-16 11:49:23 +0100

    read more/less

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,20 +1,26 @@
 import { motion } from "framer-motion";
+import { useState } from "react";
 import { fadeIn } from "../utils/animationVariants";
 import PropTypes from "prop-types";
 import cardStyles from "../styles/Card.module.css";
-const Card = ({ index,id, title, clr, content }) => {
+const Card = ({ index, id, title, clr, content }) => {
+  const [isDropped, setIsDropped] = useState(false);
+
+  const toggleDrop = () => {
+    setIsDropped((prevState) => !prevState);
+  };
   return (
     <motion.div
       variants={fadeIn("up", "spring", index * 0.5, 0.75)}
-      className={cardStyles.drop}
+      className={`${cardStyles.drop} ${isDropped ? cardStyles.active : ""}`}
       style={{ "--clr": clr }}
     >
       <div className={cardStyles.content}>
         <h2>0{id}</h2>
         <h4>{title}</h4>
-       
-          <p>{content}</p>
-        <a href="#">Read More</a>
+
+        <p>{content}</p>
+        <a href="#" className={cardStyles.more} onClick={toggleDrop}></a>
       </div>
     </motion.div>
   );
